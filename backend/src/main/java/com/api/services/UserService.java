d69dda3f44bdc0248be5fa38d6670c177151d0fe
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-11-24 12:31:42 -0800

    commit more user functionality

--- backend/src/main/java/com/api/services/UserService.java
+++ backend/src/main/java/com/api/services/UserService.java
@@ -20,12 +20,12 @@ public interface UserService extends CassandraRepository<User, UUID> {
     @Query("UPDATE coffeeshop.users SET first_name = :first_name, last_name = :last_name, email = :email WHERE id = :id IF EXISTS")
     boolean updateUser(UUID id, String first_name, String last_name, String email);
 
-    @Query("UPDATE coffeeshop.users (role) SET (:role) WHERE id = :id")
+    @Query("UPDATE coffeeshop.users SET role = :role WHERE id = :id")
     boolean updateRole(UUID id, String role);
     
-    @Query("UPDATE coffeeshop.users (password) SET (:password) WHERE id = :id")
-    boolean updatePassword(UUID id, String password);
+    // @Query("UPDATE coffeeshop.users SET password = :password WHERE id = :id")
+    // boolean updatePassword(UUID id, String password);
 
-    @Query("DELETE FROM coffeeshop.users WHERE id = :id")
-    boolean deleteUser(UUID id);
+    // @Query("DELETE FROM coffeeshop.users WHERE id = :id")
+    // boolean deleteUser(UUID id);
 }
