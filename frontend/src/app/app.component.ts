6f3281286b15fc2c2adf9dca185759490183b1fd
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-11-25 13:29:24 -0800

    click to make editable

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { ProductService } from './services/product.service';
 import { Product } from './models/Product.model';
 
@@ -11,6 +11,11 @@ export class AppComponent {
   title = 'Coffee Shop';
   products: Product[];
 
+  edit_index: Number;
+  edit_name: String;
+
+  new_value;
+
   constructor(private productService: ProductService) {
     console.log(productService.getProducts());
   }
@@ -22,4 +27,24 @@ export class AppComponent {
       }
     );
   }
+
+  onClick(index, name) {
+    console.log(index + " " + name);
+    this.edit_index = index;
+    this.edit_name = name;
+    if(name == "name") {
+      this.new_value = this.products[index].name;
+    } else if(name == "size") {
+      this.new_value = this.products[index].size;
+    } else if(name == "price") {
+      this.new_value = this.products[index].price;
+    }
+  }
+
+  onKeydown() {
+    this.edit_index = -1;
+    this.edit_name = "";
+    this.new_value = undefined;
+  }
+
 }
